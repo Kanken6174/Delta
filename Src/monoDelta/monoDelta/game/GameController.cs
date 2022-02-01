ab8a80566d5776f06f7fe1992cab6ebbecc0a0da
Onyx ab3q <Onyxab3q@outlook.com>
2022-02-01 23:18:17 +0100

    final commit of the session

--- Src/monoDelta/monoDelta/game/GameController.cs
+++ Src/monoDelta/monoDelta/game/GameController.cs
@@ -12,10 +12,48 @@ namespace monoDelta.game
     {
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
-        Vector2 baseScreenSize = new Vector2(800, 480);
+        Vector2 baseScreenSize = new Vector2(1600, 900);
         private Matrix globalTransformation;
         int backbufferWidth, backbufferHeight;
 
+        private kinect.KinectManager km;
 
+        public GameController()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.IsFullScreen = false;
+            graphics.PreferredBackBufferWidth = 1600;
+            graphics.PreferredBackBufferHeight = 900;
+            km = new kinect.KinectManager();
+        }
+
+        /// <summary>
+        /// Charge les différentes ressources du jeu (fichiers)
+        /// </summary>
+        protected override void LoadContent()
+        {
+            this.Content.RootDirectory = "ressources";
+            base.LoadContent();
+        }
+        /// <summary>
+        /// met à jour le jeu selon une vitesse de tick prédéterminée
+        /// </summary>
+        /// <param name="gameTime"></param>
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public void ScalePresentationArea()
+        {
+            //Work out how much we need to scale our graphics to fill the screen
+            backbufferWidth = GraphicsDevice.PresentationParameters.BackBufferWidth;
+            backbufferHeight = GraphicsDevice.PresentationParameters.BackBufferHeight;
+            float horScaling = backbufferWidth / baseScreenSize.X;
+            float verScaling = backbufferHeight / baseScreenSize.Y;
+            Vector3 screenScalingFactor = new Vector3(horScaling, verScaling, 1);
+            globalTransformation = Matrix.CreateScale(screenScalingFactor);
+            
+        }
     }
 }
